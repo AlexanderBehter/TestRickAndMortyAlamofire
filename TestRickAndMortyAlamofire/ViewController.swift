14c3d8fd39eed16103092df9da52ac2910de01b0
AllSymmons <behter1995@icloud.com>
2020-07-29 16:30:54 +0300

    Initial Commit

---
+++ TestRickAndMortyAlamofire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestRickAndMortyAlamofire
+//
+//  Created by Александр Бехтер on 29.07.2020.
+//  Copyright © 2020 Александр Бехтер. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
